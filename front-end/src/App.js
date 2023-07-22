ecc18f2543bdb862db3fc03b7708bbf3443887d4
Raunak Shrestha <raunaknagendra@gmail.com>
2023-07-22 23:40:36 +0545

    Added basic Nav and Footer components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import Nav from './Nav'
+import Nav from './components/Nav'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-
+import Footer from './components/Footer'
 function App() {
   return (
     <div className='App'>
@@ -16,6 +16,7 @@ function App() {
       <Route path="/profile" element={<h1>Profile  Components</h1>}/>
     </Routes>
     </BrowserRouter>
+    <Footer />
     </div>
   );
 }
