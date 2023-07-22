9567ca93b0d162d13121f181d327a3df80dba225
Raunak Shrestha <raunaknagendra@gmail.com>
2023-07-22 23:13:33 +0545

    Added Routing in front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import Nav from './Nav'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-    <h1>E-DashBoard</h1>
-    </>
+    <div className='App'>
+      <BrowserRouter>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<h1>Product Listing Components</h1>}/>
+      <Route path="/add" element={<h1>Add Product Components</h1>}/>
+      <Route path="/update" element={<h1>Update Product Components</h1>}/>
+      <Route path="/logout" element={<h1>Logout  Components</h1>}/>
+      <Route path="/profile" element={<h1>Profile  Components</h1>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
 
