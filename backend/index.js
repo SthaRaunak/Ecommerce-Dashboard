611107d3594c9a9a333fb64cd8c31e58d56c3485
Raunak Shrestha <raunaknagendra@gmail.com>
2023-07-23 13:13:46 +0545

    Connected  MongoDB with backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
-const { response } = require('express');
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("app is working...")
-})
+const connectDB = async () =>{
+    mongoose.connect('mongodb://localhost:27017/e-comm')
+    const productSchema = new mongoose.Schema({})
+    const product = mongoose.model('product',productSchema)
+    const data = await product.find()
+    console.log(data);
+}
+
+connectDB()
 
 app.listen(4000)
