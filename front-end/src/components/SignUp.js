6322c5f67df94585ea7b2408dd2ec7d767d482be
Raunak Shrestha <raunaknagendra@gmail.com>
2023-07-28 23:42:44 +0545

    Integrated SignUp API in React

--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -1,14 +1,26 @@
 import React,{useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
-const SignUp = () =>{
+const SignUp = () => {
     const[name,setName]=useState("");
     const[password,setPassword]=useState("")
     const[email,setEmail]=useState("")
+    const navigate = useNavigate();
 
-    const collectData= () =>{
+    const collectData= async() =>{
         console.log(name,email,password)
+        let result = await fetch('http://localhost:4000/signup',{
+            method: 'post',
+            body: JSON.stringify({name,email,password}),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        result = await result.json();
+        console.log(result);
+        if(result){ navigate('/')}
+    
     }
-
 
     return(
         <div className='signUp'>
@@ -20,8 +32,9 @@ const SignUp = () =>{
             <input className="inputBox" type="password"
             value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password"/>
             <button type="button" onClick={collectData}>Sign Up</button>
-            </div>
+        </div>
     )
 }
+
 
 export default SignUp
