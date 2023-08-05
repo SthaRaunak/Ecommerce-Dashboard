e06cf0a555c12a6668d58983dc128e9b3172acb3
Raunak Shrestha <raunaknagendra@gmail.com>
2023-08-05 23:48:52 +0545

    Added Login API

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,23 @@ app.use(cors())
 app.post('/signup', async (req, res) => {
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password
     res.send(result);
 })
 
+app.post('/login',async(req,res)=>{
+    console.log(req.body)
+    if(req.body.password && req.body.email){
+        let user = await User.findOne(req.body).select("-password");
+        if(user){
+            res.send(user);
+        }
+       
+    } else{
+        res.send({result:'No User Found'})
+    }
+   
+})
 
 app.listen(4000);
