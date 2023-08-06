16e95d0d8ea67fbd18fde3fcbbcf2d18ca059f3c
Raunak Shrestha <raunaknagendra@gmail.com>
2023-08-06 23:58:24 +0545

    Updated Navbar

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,35 +1,34 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const Nav = () => {
     const auth = localStorage.getItem('user')
     const navigate = useNavigate();
-    const logout = () =>{
+    const logout = () => {
         localStorage.clear();
         navigate('/signup')
     }
-useEffect(()=>{
-    const auth = localStorage.getItem('user');
-    if(auth){
-        navigate('/')
-    }
-})
+    useEffect(() => {
+        const auth = localStorage.getItem('user');
+        if (auth) {
+            navigate('/')
+        }
+    })
     return (
         <div>
-            <ul className="navItems">
+            <img alt='logo' className="logo" src='https://seeklogo.com/images/E/ecom-logo-DF4A3FF5B3-seeklogo.com.png'/>
+            {auth ? <ul className="navItems">
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                {/* <li>{auth ? <Link onClick={logout} to="/signup">Logout</Link> : <Link to="/signup">Sign Up</Link>}</li>
-                <li><Link to="/login">Log In</Link></li> */}
-                {
-                    auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>
-                    :<>
-                    <li><Link to="/signup">Sign Up</Link></li>
-                    <li><Link to ="/login">Log In</Link></li>
-                    </>
-                }
+                <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
             </ul>
+                :
+                <ul className="navItems nav-right">
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to="/login">Log In</Link></li>
+                </ul>
+            }
         </div>
     )
 }
