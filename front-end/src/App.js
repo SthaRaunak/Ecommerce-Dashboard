8e8f89799c84c4ead87f691727713ded23ae5dfb
Raunak Shrestha <raunaknagendra@gmail.com>
2023-08-06 23:08:40 +0545

    Added Login Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import SignUp from './components/SignUp'
 import PrivateComponent from './components/PrivateComponent';
-
+import Login from './components/Login'
 function App() {
   return (
     <div className='App'>
@@ -22,6 +22,7 @@ function App() {
 
           </Route>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
       <Footer />
