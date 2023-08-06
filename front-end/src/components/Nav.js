072a57abb5adea3210e2c48b47e1bd423544d56a
Raunak Shrestha <raunaknagendra@gmail.com>
2023-08-06 23:49:24 +0545

    Integrated Login API

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Link, useNavigate} from 'react-router-dom'
 const Nav = () => {
     const auth = localStorage.getItem('user')
     const navigate = useNavigate();
@@ -7,6 +7,12 @@ const Nav = () => {
         localStorage.clear();
         navigate('/signup')
     }
+useEffect(()=>{
+    const auth = localStorage.getItem('user');
+    if(auth){
+        navigate('/')
+    }
+})
     return (
         <div>
             <ul className="navItems">
@@ -14,8 +20,15 @@ const Nav = () => {
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li>{auth ? <Link onClick={logout} to="/signup">Logout</Link> : <Link to="/signup">Sign Up</Link>}</li>
-                <li><Link to="/login">Log In</Link></li>
+                {/* <li>{auth ? <Link onClick={logout} to="/signup">Logout</Link> : <Link to="/signup">Sign Up</Link>}</li>
+                <li><Link to="/login">Log In</Link></li> */}
+                {
+                    auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>
+                    :<>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to ="/login">Log In</Link></li>
+                    </>
+                }
             </ul>
         </div>
     )
